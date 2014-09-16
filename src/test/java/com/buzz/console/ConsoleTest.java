3a55491b66521911e2daec18491790eacf813c0a
James Gough <jpgough@gmail.com>
2014-09-16 15:25:32 +0530

    Initial commit of project, introduce first test stub

---
+++ src/test/java/com/buzz/console/ConsoleTest.java
@@ -0,0 +1,12 @@
+package com.buzz.console;
+
+import org.junit.Test;
+
+public class ConsoleTest {
+
+    @Test public void
+    console_call_buzz_api_with_user_and_message_entered() {
+
+    }
+
+}
